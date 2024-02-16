90a695c77a40ba42953c69e7434a53ca4aa3adc7
Bruno Azevedo <brunoacev@gmail.com>
2024-02-16 17:50:21 -0300

    Fix: Change size for mobile view

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,26 +2,24 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="flex flex-col gap-4  w-full  h-fit items-center py-5  border-t-[1px] border-zinc-300 dark:border-zinc-600 text-sm">
-      <div className="flex gap-4">
+    <footer className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:place-items-start gap-4 w-full h-fit items-start py-5 px-6  border-t-[1px] border-zinc-300 dark:border-zinc-600 text-sm">
+      <div className="flex flex-col gap-2">
         <span>Copyright &copy; 2023 War Games Contributors</span>
         <span>Released under the MIT License.</span>
       </div>
 
-      <div className="flex gap-4 items-center">
-        <div className="flex gap-4">
-          <span>Termos de Serviços</span>
-          <span>Politicas de Privacidade</span>
-        </div>
-
-        <Link
-          href={"https://brunoacev-dev.vercel.app/"}
-          target="_blank"
-          className="px-3 py-2 bg-zinc-100 text-zinc-600 hover:text-zinc-50 dark:bg-zinc-700 dark:text-zinc-50 rounded-sm font-semibold text-sm hover:cursor-pointer hover:bg-red-500 dark:hover:bg-red-500 transition-all duration-300 ease-in-out"
-        >
-          Develop by brunoacev-dev
-        </Link>
+      <div className="flex flex-col gap-2">
+        <span>Termos de Serviços</span>
+        <span>Politicas de Privacidade</span>
       </div>
+
+      <Link
+        href={"https://brunoacev-dev.vercel.app/"}
+        target="_blank"
+        className="px-3 py-2 bg-zinc-100 w-fit h-fit text-zinc-600 hover:text-zinc-50 dark:bg-zinc-700 dark:text-zinc-50 rounded-sm font-semibold text-sm hover:cursor-pointer hover:bg-red-500 dark:hover:bg-red-500 transition-all duration-300 ease-in-out"
+      >
+        Develop by brunoacev-dev
+      </Link>
     </footer>
   );
 };
